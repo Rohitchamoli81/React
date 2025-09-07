9771052569673ab609b6fc86ad72874c0c4c5202
Rohit <chamolirohit29@gmail.com>
2025-09-07 23:43:58 +0530

    change

--- 02Counter/src/App.jsx
+++ 02Counter/src/App.jsx
@@ -4,23 +4,32 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  let [counter,setcounter]=useState(15)
+  const [counter,setcounter]=useState(15)
 //let counter = 5
 
 const addValue = ()=>{
-  counter=counter+1
+  // counter=counter+1
   // console.log(Math.random());
   // console.log(`counter ${counter}`);
    if(counter<=20){
-    setcounter(counter)
+    // setcounter(counter)
+    // setcounter(counter)
+    /* setcounter(counter) as many time only one value will increase because react update in bunch of function so they all are preforming 
+    same task . */
+    // setcounter(counter)
+
+    // But to update counter value like this we can use callback function which is given bysetcounter
+    setcounter(PrevCounter=>PrevCounter+1)
+    setcounter(PrevCounter=>PrevCounter+1)
+    setcounter(PrevCounter=>PrevCounter+1)
+    setcounter(PrevCounter=>PrevCounter+1)
    }
   
 }
 
 const removeValue = ()=>{
-  counter=counter-1
-if(counter>=0){
-  setcounter(counter)
+if(counter>0){
+  setcounter(counter-1)
 }
 }
 
